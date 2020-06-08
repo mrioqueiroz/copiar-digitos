f5ae654fcbe219e950b4091d8e9bbf7bd0679c7e
Mário Queiroz <mario.al.queiroz@gmail.com>
2020-06-07 22:07:16 -0300

    Change the context in which the menu appears
    Also, add the tabCapture permission to read and change data
    on the page.

--- background.js
+++ background.js
@@ -2,7 +2,10 @@ var contextMenus = {};
 
 contextMenus.copyDigits =
     chrome.contextMenus.create(
-        {"title": "Copiar dígitos"},
+        {
+            "title": "Copiar dígitos",
+            "contexts": ["selection"]
+        },
         function() {
             if (chrome.runtime.lastError) {
                 console.error(chrome.runtime.lastError.message)
